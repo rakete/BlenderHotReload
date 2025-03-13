ebf73e890268c6a174d8832a1fff2cc90e64e1ea
Andreas Raster <lazor@affenbande.org>
2025-03-13 03:21:56 +0100

    add buttons for enabling/disabling polling to VIEW3D header, add nicer reporting

--- __init__.py
+++ __init__.py
@@ -15,24 +15,63 @@ bl_info = {
 import bpy
 import importlib
 
-from BlenderHotReload.reload import StartPollingOperator, StopPollingOperator, WATCHED_DIR
+from .reload import (
+    get_polling_active,
+    set_polling_active,
+    HOTRELOAD_OT_start_polling_operator,
+    HOTRELOAD_OT_stop_polling_operator,
+    WATCHED_DIR,
+)
+from .settings import HOTRELOAD_OT_check_go_and_install
+
+class HotReloadAddonPreferences(bpy.types.AddonPreferences):
+    bl_idname = __package__
+
+def draw_hotreload_buttons(self, context):
+    layout = self.layout
+    row = layout.row(align=True)
+
+    is_polling = get_polling_active()
+
+    c1 = row.column(align=True)
+    c1.operator("hotreload.start_polling", text="Hot Reload", icon='FILE_REFRESH', depress=is_polling)
+    c2 = row.column(align=True)
+    c2.operator("hotreload.stop_polling", text="", icon='CANCEL')
+    c2.enabled = is_polling
 
 def register():
     if not WATCHED_DIR:
         print("HotReload: No directory or module specified. Exiting.")
         return
 
-    bpy.utils.register_class(StartPollingOperator)
-    bpy.utils.register_class(StopPollingOperator)
+    bpy.utils.register_class(HotReloadAddonPreferences)
+    bpy.utils.register_class(HOTRELOAD_OT_start_polling_operator)
+    bpy.utils.register_class(HOTRELOAD_OT_stop_polling_operator)
+
+    bpy.utils.register_class(HOTRELOAD_OT_check_go_and_install)
+
+    bpy.types.VIEW3D_HT_header.append(draw_hotreload_buttons)
+
+    # If you want to add it to other headers as well:
+    # bpy.types.TOPBAR_HT_upper_bar.append(draw_hotreload_button)  # Top-most header
+    # bpy.types.TEXT_HT_header.append(draw_hotreload_button)  # Text editor header
+    # bpy.types.NODE_HT_header.append(draw_hotreload_button)  # Node editor header
+
 
 def unregister():
     if not WATCHED_DIR:
         return
 
-    bpy.ops.wm.stop_polling()
+    bpy.ops.hotreload.stop_polling()
 
-    bpy.utils.unregister_class(StartPollingOperator)
-    bpy.utils.unregister_class(StopPollingOperator)
+    bpy.types.VIEW3D_HT_header.remove(draw_hotreload_buttons)
+
+    bpy.utils.unregister_class(HOTRELOAD_OT_start_polling_operator)
+    bpy.utils.unregister_class(HOTRELOAD_OT_stop_polling_operator)
+
+    bpy.utils.unregister_class(HOTRELOAD_OT_check_go_and_install)
+
+    bpy.utils.unregister_class(HotReloadAddonPreferences)
 
 if __name__ == "__main__":
     register()
--- reload.py
+++ reload.py
@@ -11,58 +11,67 @@ WATCHED_DIR = os.getenv("HOTRELOAD_WATCHED_DIR", "")
 # Store the last modification time and changed files
 last_mod_time = datetime.now().timestamp()
 
+hotreload_polling_active = False
 
-def check_for_changes():
-    global last_mod_time
+def set_polling_active(state):
+    global hotreload_polling_active
+    hotreload_polling_active = state
 
-    hotreload_path = os.path.join(WATCHED_DIR, ".hotreload")
-    if os.path.exists(hotreload_path):
-        current_mod_time = os.path.getmtime(hotreload_path)
-
-        if current_mod_time > last_mod_time:
-            print("Detected change in", hotreload_path)
-            with open(hotreload_path, "r") as f:
-                last_mod_time = current_mod_time
-                data = json.load(f)
-                last_change = data.get("last_change")
-                watched_dirs = data.get("watched_dirs")
-                for watched_dir_and_module_names in watched_dirs:
-                    watched_dir_split = watched_dir_and_module_names.split("|")
-                    if len(watched_dir_split) > 1:
-                        watched_dir, module_names = watched_dir_split
-                        changed_dir = last_change.get("changed_dir")
-                        if (changed_dir == "." and watched_dir == ".") or os.path.abspath(watched_dir) == os.path.abspath(changed_dir):
-                            for module_name in module_names.split(","):
-                                reload_module(module_name)
-
-def reload_module(module_name):
-    try:
-        # Disable the addon
-        print("Disabling", module_name)
-        bpy.ops.preferences.addon_disable(module=module_name)
-
-        # Delete the module from sys.modules
-        for name in list(sys.modules.keys()):
-            if name == module_name or name.startswith(module_name + "."):
-                print(f"Deleting module: {name}")
-                del sys.modules[name]
-
-        # Enable the addon
-        print("Enabling", module_name)
-        bpy.ops.preferences.addon_enable(module=module_name)
-
-        print(f"Reloaded module: {module_name}")
-    except Exception as e:
-        print(f"Failed to reload module: {e}")
+def get_polling_active():
+    global hotreload_polling_active
+    return hotreload_polling_active
 
 timer = None
 wait_time = 2.0
 
-class StartPollingOperator(bpy.types.Operator):
-    bl_idname = "wm.start_polling"
+class HOTRELOAD_OT_start_polling_operator(bpy.types.Operator):
+    bl_idname = "hotreload.start_polling"
     bl_label = "Start Hot Reload Polling"
 
     duration = None
+
+    def check_for_changes(self):
+        global last_mod_time
+
+        hotreload_path = os.path.join(WATCHED_DIR, ".hotreload")
+        if os.path.exists(hotreload_path):
+            current_mod_time = os.path.getmtime(hotreload_path)
+
+            if current_mod_time > last_mod_time:
+                print("Detected change in", hotreload_path)
+                with open(hotreload_path, "r") as f:
+                    last_mod_time = current_mod_time
+                    data = json.load(f)
+                    last_change = data.get("last_change")
+                    watched_dirs = data.get("watched_dirs")
+                    for watched_dir_and_module_names in watched_dirs:
+                        watched_dir_split = watched_dir_and_module_names.split("|")
+                        if len(watched_dir_split) > 1:
+                            watched_dir, module_names = watched_dir_split
+                            changed_dir = last_change.get("changed_dir")
+                            if (changed_dir == "." and watched_dir == ".") or os.path.abspath(watched_dir) == os.path.abspath(changed_dir):
+                                for module_name in module_names.split(","):
+                                    self.reload_module(module_name)
+
+    def reload_module(self, module_name):
+        try:
+            # Disable the addon
+            print("Disabling", module_name)
+            bpy.ops.preferences.addon_disable(module=module_name)
+
+            # Delete the module from sys.modules
+            for name in list(sys.modules.keys()):
+                if name == module_name or name.startswith(module_name + "."):
+                    print(f"Deleting module: {name}")
+                    del sys.modules[name]
+
+            # Enable the addon
+            print("Enabling", module_name)
+            bpy.ops.preferences.addon_enable(module=module_name)
+
+            self.report({'INFO'}, f"Reloaded module: {module_name}")
+        except Exception as e:
+            self.report({'ERROR'}, f"Failed to reload module: {e}")
 
     def modal(self, context, event):
         global timer, wait_time
@@ -70,35 +79,37 @@ class StartPollingOperator(bpy.types.Operator):
         if self.duration is not None:
             elapsed = datetime.now() - self.duration
         if timer is not None and event.type == 'TIMER' and elapsed and elapsed.total_seconds() >= wait_time:
-            check_for_changes()
+            self.check_for_changes()
             self.duration = datetime.now()
         return {'PASS_THROUGH'}
 
     def execute(self, context):
-        print("Starting polling")
-
         global last_mod_time, changed_files, timer
         if timer is not None:
             return {'CANCELLED'}
 
+        set_polling_active(True)
+        self.report({'INFO'}, f"Polling for changes in {WATCHED_DIR} every {wait_time} seconds")
+
         self.duration = datetime.now()
         last_mod_time = time.time()
         changed_files = []
-        wm = context.window_manager
+        wm = bpy.context.window_manager
         timer = wm.event_timer_add(wait_time, window=context.window)
         wm.modal_handler_add(self)
         return {'RUNNING_MODAL'}
 
-class StopPollingOperator(bpy.types.Operator):
-    bl_idname = "wm.stop_polling"
+class HOTRELOAD_OT_stop_polling_operator(bpy.types.Operator):
+    bl_idname = "hotreload.stop_polling"
     bl_label = "Stop Hot Reload Polling"
 
     def execute(self, context):
-        print("Stopping polling")
-
         global timer
         if timer is None:
             return {'CANCELLED'}
+
+        set_polling_active(False)
+        self.report({'INFO'}, f"Stopped polling")
 
         wm = context.window_manager
         wm.event_timer_remove(timer)
